deb7f4e8b96d76ab73ead97120af7f95a50e3783
Alex <alex_deng22@outlook.com>
2023-01-13 12:56:13 +0000

    style: layout and colours

--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -26,33 +26,55 @@ function Buttons(props) {
 
   return (
     <div className="buttons">
-      <button onClick={() => addChar("Abs")}>Abs</button>
+      <button className="other" onClick={() => addChar("Abs")}>
+        Abs
+      </button>
       {shift ? (
-        <button onClick={() => addChar("sin")}>sin</button>
+        <button className="trig" onClick={() => addChar("sin")}>
+          sin
+        </button>
       ) : (
-        <button onClick={() => addChar("asin")}>asin</button>
+        <button className="trig" onClick={() => addChar("asin")}>
+          asin
+        </button>
       )}
       {shift ? (
-        <button onClick={() => addChar("cos")}>cos</button>
+        <button className="trig" onClick={() => addChar("cos")}>
+          cos
+        </button>
       ) : (
-        <button onClick={() => addChar("acos")}>acos</button>
+        <button className="trig" onClick={() => addChar("acos")}>
+          acos
+        </button>
       )}
       {shift ? (
-        <button onClick={() => addChar("tan")}>tan</button>
+        <button className="trig" onClick={() => addChar("tan")}>
+          tan
+        </button>
       ) : (
-        <button onClick={() => addChar("atan")}>atan</button>
+        <button className="trig" onClick={() => addChar("atan")}>
+          atan
+        </button>
       )}
-      <button onClick={() => setShift(!shift)}>shift</button>
+      <button className="trig" onClick={() => setShift(!shift)}>
+        shift
+      </button>
 
+      <button className="other" onClick={() => addChar("E")}>
+        EXP
+      </button>
+      <button className="other" onClick={() => addChar("!")}>
+        !
+      </button>
+      <button className="other" onClick={() => addChar("ln")}>
+        ln
+      </button>
       <button className="bracket-button" onClick={() => addChar("(")}>
         (
       </button>
       <button className="bracket-button" onClick={() => addChar(")")}>
         )
       </button>
-      <button onClick={() => addChar("π")}>π</button>
-      <button onClick={() => addChar("e")}>e</button>
-      <button onClick={() => addChar("ln")}>ln</button>
 
       <button onClick={() => addChar("7")}>7</button>
       <button onClick={() => addChar("8")}>8</button>
@@ -67,20 +89,34 @@ function Buttons(props) {
       <button onClick={() => addChar("4")}>4</button>
       <button onClick={() => addChar("5")}>5</button>
       <button onClick={() => addChar("6")}>6</button>
-      <button onClick={() => addChar("×")}>×</button>
-      <button onClick={() => addChar("÷")}>÷</button>
+      <button className="elementary" onClick={() => addChar("×")}>
+        ×
+      </button>
+      <button className="elementary" onClick={() => addChar("÷")}>
+        ÷
+      </button>
 
       <button onClick={() => addChar("1")}>1</button>
       <button onClick={() => addChar("2")}>2</button>
       <button onClick={() => addChar("3")}>3</button>
-      <button onClick={() => addChar("+")}>+</button>
-      <button onClick={() => addChar("-")}>-</button>
+      <button className="elementary" onClick={() => addChar("+")}>
+        +
+      </button>
+      <button className="elementary" onClick={() => addChar("-")}>
+        -
+      </button>
 
       <button onClick={() => addChar("0")}>0</button>
       <button onClick={() => addChar(".")}>.</button>
-      <button onClick={() => addChar("E")}>EXP</button>
-      <button onClick={() => addChar("^")}>^</button>
-      <button onClick={() => addChar("!")}>!</button>
+      <button className="constant" onClick={() => addChar("π")}>
+        π
+      </button>
+      <button className="constant" onClick={() => addChar("e")}>
+        e
+      </button>
+      <button className="elementary" onClick={() => addChar("^")}>
+        ^
+      </button>
     </div>
   );
 }
